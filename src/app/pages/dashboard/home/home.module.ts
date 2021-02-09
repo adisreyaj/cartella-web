4f23d41a1c6bcc162c429e20f23b1f55ceef59f7
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-02-09 18:11:40 +0530

    feat(home): refactor components added service

--- src/app/pages/dashboard/home/home.module.ts
+++ src/app/pages/dashboard/home/home.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import { IconModule } from '@app/modules/icon/icon.module';
 import { ButtonsModule } from 'projects/ui/src/public-api';
 import { SwiperModule } from 'swiper/angular';
-import { HomeItemCardComponent } from './components/home-item-card/home-item-card.component';
-import { HomeMostRecentlyAddedComponent } from './components/home-most-recently-added/home-most-recently-added.component';
-import { HomeTopItemsComponent } from './components/home-top-items/home-top-items.component';
-import { StatsCardComponent } from './components/stats-card/stats-card.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { HomeItemCardComponent } from './shared/components/home-item-card/home-item-card.component';
+import { HomeMostRecentlyAddedComponent } from './shared/components/home-most-recently-added/home-most-recently-added.component';
+import { HomeTopItemsComponent } from './shared/components/home-top-items/home-top-items.component';
+import { StatsCardComponent } from './shared/components/stats-card/stats-card.component';
 
 @NgModule({
   declarations: [
