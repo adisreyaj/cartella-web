65035130855d8a8797cd9361462c67fa62084469
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-02-11 20:14:13 +0530

    refactor: remove custom webpack

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-const { patchPostCSS } = require('@ngneat/tailwind');
-
-module.exports = (config) => {
-  const tailwindConfig = require('./tailwind.config.js');
-  patchPostCSS(config, tailwindConfig, true);
-  return config;
-};
