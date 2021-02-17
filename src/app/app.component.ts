c28d7f051a3a22fa8010a62b2ee895f04b90c0db
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-02-17 21:31:40 +0530

    style(dark): add dark mode to auth pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { DarkModeService } from '@app/services/dark-mode/dark-mode.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  // To enable dark mode in auth pages
+  constructor(private dark: DarkModeService) {}
+}
