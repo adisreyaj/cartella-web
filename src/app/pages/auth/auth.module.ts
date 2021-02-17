1bc0655dd133914f25729fbc65cf49ee00a7253f
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-02-17 23:24:48 +0530

    feat(config): signup now based on feature flag

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FeatureDirectiveModule } from '@app/directives/feature/feature.module';
 import { ButtonsModule } from 'projects/ui/src/public-api';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginHandlerComponent } from './login-handler/login-handler.component';
@@ -14,6 +15,7 @@ import { SignUpComponent } from './signup/signup.component';
     AuthRoutingModule,
     ButtonsModule,
     ReactiveFormsModule,
+    FeatureDirectiveModule,
   ],
 })
 export class AuthModule {}
