419562bcd743e1d0b7f143cf01ade47652375a89
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-02-20 15:52:02 +0530

    Dev (#63)
    * feat(meta): update meta tags for each routes
    
    * refactor: replace canActivate with canLoad guards

--- src/app/shared/guards/feature.guard.spec.ts
+++ src/app/shared/guards/feature.guard.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { FeatureGuard } from './feature.guard';
 
 describe('FeatureGuard', () => {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
+  hostname: 'https://cartella.sreyaj.dev',
   api: 'https://srv.sreyaj.dev/cartella/api',
 };
