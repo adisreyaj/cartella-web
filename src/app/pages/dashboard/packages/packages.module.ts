1bd03310f00ea493c058c660a0864a21b0074d9e
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-02-26 20:07:41 +0530

    refactor(packages): restructured components

--- src/app/pages/dashboard/packages/packages.module.ts
+++ src/app/pages/dashboard/packages/packages.module.ts
@@ -12,7 +12,7 @@ import { ButtonsModule } from 'projects/ui/src/public-api';
 import { ExplorerSidebarModule } from '../shared/components/explorer-sidebar/explorer-sidebar.module';
 import { PackagesAddFolderComponent } from './components/modals/packages-add-folder/packages-add-folder.component';
 import { PackagesAddComponent } from './components/modals/packages-add/packages-add.component';
-import { PackagesListCardComponent } from './components/packages-list/packages-list-card/packages-list-card.component';
+import { PackagesListCardComponent } from './components/packages-list-card/packages-list-card.component';
 import { PackagesListComponent } from './components/packages-list/packages-list.component';
 import { PackagesRoutingModule } from './packages-routing.module';
 import { PackagesComponent } from './packages.component';
