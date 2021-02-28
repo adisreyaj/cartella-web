ac5350225092db84aef04013d3f19d12d0affb75
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-02-28 12:59:30 +0530

    feat(auth): add loader in login button

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastService } from '@app/services/toast/toast.service';
+import { BehaviorSubject } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { AuthService } from '../../../shared/services/auth/auth.service';
 @Component({
@@ -12,6 +13,9 @@ import { AuthService } from '../../../shared/services/auth/auth.service';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
+
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+  readonly loading$ = this.loadingSubject.pipe();
 
   constructor(
     private auth: AuthService,
@@ -27,6 +31,7 @@ export class LoginComponent implements OnInit {
   signInWithCredentials() {
     if (this.loginForm.valid) {
       const value = this.loginForm.value;
+      this.loadingSubject.next(true);
       this.auth
         .singInWithUserCredentials(value)
         .pipe(
@@ -38,9 +43,11 @@ export class LoginComponent implements OnInit {
         )
         .subscribe(
           () => {
+            this.loadingSubject.next(false);
             this.router.navigate(['/']);
           },
           (error) => {
+            this.loadingSubject.next(false);
             this.toast.showErrorToast(error.error.message);
           }
         );
@@ -48,9 +55,11 @@ export class LoginComponent implements OnInit {
   }
 
   signInWithGoogle() {
+    this.loadingSubject.next(true);
     this.auth.signInWithGoogle();
   }
   async signInWithGithub() {
+    this.loadingSubject.next(true);
     this.auth.signInWithGithub();
   }
   private initForm() {
