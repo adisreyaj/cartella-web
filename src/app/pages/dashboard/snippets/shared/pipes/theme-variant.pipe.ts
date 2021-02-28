859b5ac36c81c9fe6d0fe6d68bfdb4fc83148356
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-02-28 13:35:18 +0530

    fix(snippets): editor theme contrast issue when toggling dark mode

--- src/app/pages/dashboard/snippets/shared/pipes/theme-variant.pipe.ts
+++ src/app/pages/dashboard/snippets/shared/pipes/theme-variant.pipe.ts
@@ -5,11 +5,23 @@ import { ThemesSupported } from '@app/config/snippets.config';
   name: 'themeVariant',
 })
 export class ThemeVariantPipe implements PipeTransform {
+  /**
+   * Return the possible themes based on if dark mode is enabled or not
+   *
+   * If Dark mode is enabled, only dark themes are available. For light mode, all themes
+   * including both light and dark variants are made available.
+   *
+   * @param value - themes supported
+   * @param isDarkMode - is dark mode enabled
+   */
   transform(value: ThemesSupported[], isDarkMode = false): unknown {
     if (value?.length > 0) {
-      return value.filter(
-        ({ type }) => type === (isDarkMode ? 'dark' : 'light')
-      );
+      return value.filter(({ type }) => {
+        if (isDarkMode) {
+          return type === 'dark';
+        }
+        return true;
+      });
     }
     return [];
   }
