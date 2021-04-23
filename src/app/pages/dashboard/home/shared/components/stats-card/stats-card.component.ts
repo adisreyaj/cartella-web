2a5f874870ee65b6c6e23c120e9cc5abb01dceaa
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-04-24 01:01:57 +0530

    fix(home): stats card not displaying value when 0

--- src/app/pages/dashboard/home/shared/components/stats-card/stats-card.component.ts
+++ src/app/pages/dashboard/home/shared/components/stats-card/stats-card.component.ts
@@ -7,13 +7,30 @@ import {
 
 @Component({
   selector: 'app-stats-card',
-  templateUrl: './stats-card.component.html',
-  styleUrls: ['./stats-card.component.scss'],
+  template: ` <div
+    class="flex items-center justify-between p-2 bg-white rounded-md shadow-md dark:bg-dark-500"
+  >
+    <div class="p-2 text-white rounded-full bg-primary">
+      <ng-content></ng-content>
+    </div>
+
+    <div class="flex flex-col items-end justify-center">
+      <ng-container *ngIf="value != null && !isLoading">
+        <p class="text-lg font-semibold text-gray-800 dark:text-white">
+          {{ value }}
+        </p>
+      </ng-container>
+      <ng-container *ngIf="isLoading">
+        <div class="w-6 h-6 mb-1 rounded-full shimmer"></div>
+      </ng-container>
+      <p class="-mt-1 text-xs text-gray-500 dark:text-gray-50">{{ label }}</p>
+    </div>
+  </div>`,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StatsCardComponent implements OnInit {
   @Input() label: string;
-  @Input() value: string | Number;
+  @Input() value: string | number;
   @Input() isLoading = false;
   constructor() {}
 
