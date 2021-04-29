16f8f88155e2b3229db7fbc69b9bcf04cde91745
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-04-30 00:12:05 +0530

    feat(sync): add cleanup service to flush local synced data

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '@cartella/interfaces/user.interface';
+import { CleanupService } from '@cartella/services/cleanup/cleanup.service';
 import { DarkModeService } from '@cartella/services/dark-mode/dark-mode.service';
 import { MenuService } from '@cartella/services/menu/menu.service';
 import { LogoutUser } from '@cartella/store/actions/user.action';
@@ -8,23 +9,26 @@ import { UserState } from '@cartella/store/states/user.state';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
+import { SubSink } from 'subsink';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   @Select(UserState.getLoggedInUser)
   user$: Observable<User>;
   isDarkMode$: Observable<boolean>;
-
   isMenuOpen$: Observable<boolean>;
+
+  private subs = new SubSink();
   constructor(
     private store: Store,
     private router: Router,
     private menu: MenuService,
-    private darkMode: DarkModeService
+    private darkMode: DarkModeService,
+    private cleanupService: CleanupService
   ) {}
 
   ngOnInit(): void {
@@ -32,17 +36,24 @@ export class HeaderComponent implements OnInit {
     this.isDarkMode$ = this.darkMode.isDarkMode$;
   }
 
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
+
   toggleDarkMode() {
     this.darkMode.toggle();
   }
 
   logout() {
-    this.store
-      .dispatch(new LogoutUser())
-      .pipe(take(1))
-      .subscribe(() => {
-        this.router.navigate(['/auth/login']);
-      });
+    this.subs.add(
+      this.store
+        .dispatch(new LogoutUser())
+        .pipe(take(1))
+        .subscribe(() => {
+          this.cleanupService.cleanUpLocalSyncedData();
+          this.router.navigate(['/auth/login']);
+        })
+    );
   }
 
   toggleMenu() {
